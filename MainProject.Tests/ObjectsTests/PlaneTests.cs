ef2a784bcb75a0763e6bdb28923ca33bc96ec323
Hurmaze <torgmas13@gmail.com>
2023-02-28 22:09:45 +0200

    Add test project

---
+++ MainProject.Tests/ObjectsTests/PlaneTests.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using MainProject.Objects;
+using NUnit.Framework;
+
+namespace MainProject.Tests.ObjectsTests
+{
+    internal class PlaneTests
+    {
+        [Test]
+        [Category("Negative")]
+        public void GetIntersectionWith_ParallelRayTest()
+        {
+            var plane = new Plane
+            {
+                Point = new Point(0, 0, 0),
+                Normal = new Vector(1, 2, 3)
+            };
+
+            var ray = new Ray
+            {
+                Origin = new Point(0, 0, 0),
+                Direction = new Vector(-2, 1, 0)
+            };
+
+            plane.GetIntersectionWith(ray).Should().BeFalse();
+        }
+
+        [Test]
+        [Category("Negative")]
+        public void GetIntersectionWith_NoIntersectionTest()
+        {
+            var plane = new Plane
+            {
+                Point = new Point(0, 0, 0),
+                Normal = new Vector(1, 2, 3)
+            };
+
+            var ray = new Ray
+            {
+                Origin = new Point(0, 0, 0),
+                Direction = new Vector(5, 1, 0)
+            };
+
+            plane.GetIntersectionWith(ray).Should().BeFalse();
+        }
+
+        [Test]
+        [Category("Positive")]
+        public void GetIntersectionWith_IntersectionTest()
+        {
+            var plane = new Plane
+            {
+                Point = new Point(0, 0, 0),
+                Normal = new Vector(3, -1, 4)
+            };
+
+            var ray = new Ray
+            {
+                Origin = new Point(0, 0, 0),
+                Direction = new Vector(-2, 3, 1)
+            };
+
+            plane.GetIntersectionWith(ray).Should().BeTrue();
+        }
+    }
+}
---
+++ MainProject.Tests/ObjectsTests/SphereTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using MainProject.Objects;
+using NUnit.Framework;
+
+namespace MainProject.Tests.ObjectsTests
+{
+    internal class SphereTests
+    {
+        [Test]
+        [Category("Negative")]
+        public void GetIntersectionWith_NoIntersectionTest()
+        {
+            var sphere = new Sphere()
+            {
+                Center = new Point(3, 3, 1),
+                Radius = 3
+            };
+
+            var ray = new Ray
+            {
+                Origin = new Point(0, 0, 0),
+                Direction = new Vector(2, 1, 4)
+            };
+
+            sphere.GetIntersectionWith(ray).Should().BeFalse();
+        }
+
+        [Test]
+        [Category("Positive")]
+        public void GetIntersectionWith_IntersectionTest()
+        {
+            var sphere = new Sphere()
+            {
+                Center = new Point(3, 3, 1),
+                Radius = 3
+            };
+
+            var ray = new Ray
+            {
+                Origin = new Point(0, 0, 0),
+                Direction = new Vector(5, 1, 0)
+            };
+
+            sphere.GetIntersectionWith(ray).Should().BeTrue();
+        }
+    }
+}
