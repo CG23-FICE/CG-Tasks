d1bea79cd1206d006a68737a00614d94cb655301
f1ctashka <1ilchenko.victoria@gmail.com>
2023-03-06 13:30:54 +0200

    added console renderer

---
+++ MainProject/Objects/ConsoleRenderer.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace MainProject.Objects
+{
+    class ConsoleRenderer
+    {
+        public static void Main(int[,] pixels)
+        {
+            for (int i = 0; i < Array.GetLength(0); i++)
+            {
+                for (int j = 0; j < Array.GetLength(1); j++)
+                {
+                    if (pixels[i, j] < 0)
+                    {
+                        Console.Write(" ");
+                    }
+                    else if (pixels[i, j] >= 0 && pixels[i, j] < 0.2)
+                    {
+                        Console.Write(".");
+                    }
+                    else if (pixels[i, j] >= 0.2 && pixels[i, j] < 0.5)
+                    {
+                        Console.Write("*");
+                    }
+                    else if (pixels[i, j] >= 0.5 && pixels[i, j] < 0.8)
+                    {
+                        Console.Write("O");
+                    }
+                    else
+                    {
+                        Console.Write("#");
+                    }
+                }
+                Console.WriteLine();
+            }
+     
+        }
+    }
+}
