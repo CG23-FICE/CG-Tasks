fa719b4fdfde5887851d6912900b3600867c971b
Hurmaze <torgmas13@gmail.com>
2023-03-06 23:38:25 +0200

    add raytracer

---
+++ MainProject/Scene.cs
@@ -0,0 +1,23 @@
+﻿using MainProject.Interfaces;
+using MainProject.Objects;
+
+namespace MainProject
+{
+    public class Scene
+    {
+        public List<IIntersectable> Figures { get; set; }
+        private Vector _lightSource;
+        public Vector LightSource
+        {
+            get
+            {
+                return _lightSource;
+            }
+            set
+            {
+                _lightSource = value.Normalize();
+            }
+        }
+        public Camera Camera { get; set; }
+    }
+}
