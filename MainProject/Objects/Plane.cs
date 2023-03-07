c3106cde35df7e232a04016c81dc2f22b23f942d
RomanMachylo <maciloroman@gmail.com>
2023-03-07 13:12:02 +0200

    minor fix

--- MainProject/Objects/Plane.cs
+++ MainProject/Objects/Plane.cs
@@ -6,6 +6,16 @@ namespace MainProject.Objects
     {
         public Vector Normal { get; set; }
         public Point Point { get; set; }
+        public bool GetIntersectionWith(Ray ray, out Point PointOfIntersection)
+        {
+            PointOfIntersection = new Point(0, 0, 0);
+            var vectorProduct = Vector.Dot(Normal, ray.Direction);
+            if (Math.Abs(vectorProduct) < 1e-6)
+            {
+                return false;
+            }
+
+            var arccos = Math.Acos(vectorProduct / (Normal.Module() * ray.Direction.Module()));
         public Point? GetIntersectionWith(Ray ray)
         {
             throw new NotImplementedException();
@@ -22,6 +32,8 @@ namespace MainProject.Objects
             return arccos > Math.PI / 2 && arccos < Math.PI; //between 90 and 180 degrees*/
         }
 
+            return arccos > Math.PI / 2 && arccos < Math.PI; //between 90 and 180 degrees
+        }
         public Vector GetNormalAtPoint(Point point)
         {
             throw new NotImplementedException();
