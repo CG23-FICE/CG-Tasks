607225f0d1761f793c57fba5bc02778d5e89b44a
Hurmaze <torgmas13@gmail.com>
2023-03-08 21:49:25 +0200

    implement RayTracer for nearest figure

--- MainProject/Objects/Point.cs
+++ MainProject/Objects/Point.cs
@@ -15,6 +15,14 @@ namespace MainProject.Objects
         }
         public Point() { }
 
+        public static float Distance(Point point1, Point point2)
+        {
+            var a = Math.Pow(point2.X - point1.X, 2);
+            var b = Math.Pow(point2.Y - point1.Y, 2);
+            var c = Math.Pow(point2.Z - point1.Z, 2);
+            return (float)Math.Sqrt(a + b + c);
+        }
+
         public static Point operator +(Point point, Vector vector)
         {
             return new Point(point.X + vector.X, point.Y + vector.Y, point.Z + vector.Z);
