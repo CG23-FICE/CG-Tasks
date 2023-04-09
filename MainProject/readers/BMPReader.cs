6f8a68952c82d4f04677a708b7bb9efb6272c612
RomanMachylo <maciloroman@gmail.com>
2023-04-09 11:11:08 +0300

    Added bmp reader and writer

---
+++ MainProject/readers/BMPReader.cs
@@ -0,0 +1,59 @@
+﻿using ImageConverter.Sdk.Interfaces;
+using ImageConverter.Sdk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProject.Readers
+{
+    public class BMPReader : IImageReader
+    {
+        public string Title => throw new NotImplementedException();
+
+        public string Description => throw new NotImplementedException();
+
+        public string FormatName => throw new NotImplementedException();
+
+        public Bitmap Read(Stream stream)
+        {
+            using BinaryReader reader = new BinaryReader(stream);
+
+            BMPHeader Header = new BMPHeader
+            {
+                Id = reader.ReadUInt16(),
+                FileSize = reader.ReadUInt32(),
+                Reserved = reader.ReadUInt32(),
+                HeaderSize = reader.ReadUInt32(),
+                InfoSize = reader.ReadUInt32(),
+                Width = reader.ReadUInt32(),
+                Height = reader.ReadUInt32(),
+                Planes = reader.ReadUInt16(),
+                BitsPerPixel = reader.ReadUInt16(),
+                Compression = reader.ReadUInt32(),
+                ImageSize = reader.ReadUInt32(),
+                XPixelsPerMeter = reader.ReadUInt32(),
+                YPixelsPerMeter = reader.ReadUInt32(),
+                ColorsUsed = reader.ReadUInt32(),
+                ColorsImportant = reader.ReadUInt32()
+            };
+            int rowPadding = 3 - ((int)Header.Width * 3 - 1) % 4;
+            int colorTableSize = (int)(Header.HeaderSize + (Header.BitsPerPixel <= 8 ? (1 << Header.BitsPerPixel) * 4 : 0) - 54);
+            reader.ReadBytes(colorTableSize);
+            var bitmap = new Bitmap(Header.Height, Header.Width);
+
+            for (uint y = 0; y < Header.Height; y++)
+            {
+                for (uint x = 0; x < Header.Width; x++)
+                {
+                    bitmap[y, x] = new Pixel(reader.ReadByte(), reader.ReadByte(), reader.ReadByte());
+                }
+                reader.ReadBytes(rowPadding);
+            }
+
+            return bitmap;
+        }
+    }
+}
---
+++ MainProject/writers/BMPWriter.cs
@@ -0,0 +1,77 @@
+﻿using ImageConverter.Sdk.Interfaces;
+using ImageConverter.Sdk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProject.Writers
+{
+    public class BMPWriter : IImageWriter
+    {
+        public string Title => throw new NotImplementedException();
+
+        public string Description => throw new NotImplementedException();
+
+        public string FormatName => throw new NotImplementedException();
+
+        public void Write(Bitmap bitmap, string path)
+        {
+            uint rowPadding = 3 - (bitmap.Width * 3 - 1) % 4;
+            uint imageSize = bitmap.Width * bitmap.Height * 3 + rowPadding * bitmap.Height;
+            uint fileSize = imageSize + 54;
+
+            var header = new BMPHeader()
+            {
+                Id = Convert.ToByte("BM"),
+                Reserved = 0,
+                FileSize = fileSize,
+                HeaderSize = 54,
+                InfoSize = 40,
+                Width = bitmap.Width,
+                Height = bitmap.Height,
+                Planes = 1,
+                BitsPerPixel = 24,
+                Compression = 0,
+                ImageSize = imageSize,
+                XPixelsPerMeter = 0,
+                YPixelsPerMeter = 0,
+                ColorsUsed = 0,
+                ColorsImportant = 0
+            };
+
+
+            using FileStream stream = new FileStream(path, FileMode.Create);
+            using BinaryWriter writer = new BinaryWriter(stream);
+            writer.Write(header.Id);
+            writer.Write(header.FileSize);
+            writer.Write(header.Reserved);
+            writer.Write(header.HeaderSize);
+            writer.Write(header.HeaderSize);
+            writer.Write(header.Width);
+            writer.Write(header.Height);
+            writer.Write(header.Planes);
+            writer.Write(header.BitsPerPixel);
+            writer.Write(header.Compression);
+            writer.Write(header.ImageSize);
+            writer.Write(header.XPixelsPerMeter);
+            writer.Write(header.YPixelsPerMeter);
+            writer.Write(header.ColorsUsed);
+            writer.Write(header.ColorsImportant);
+            for (uint y = 0; y < header.Height; y++)
+            {
+                for (uint x = 0; x < header.Width; x++)
+                {
+                    writer.Write((byte)bitmap[y, x].R);
+                    writer.Write((byte)bitmap[y, x].G);
+                    writer.Write((byte)bitmap[y, x].B);
+                }
+                for (int j = 0; j < rowPadding; j++)
+                {
+                    writer.Write(Convert.ToByte(0));
+                }
+            }
+        }
+    }
+}
