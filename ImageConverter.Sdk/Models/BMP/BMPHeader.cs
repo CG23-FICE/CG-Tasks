d0da076de74de4d9fbac395fab91d3ee55379ec3
RomanMachylo <maciloroman@gmail.com>
2023-04-09 11:11:08 +0300

    Added bmp reader and writer

---
+++ ImageConverter.Sdk/Models/BMP/BMPHeader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageConverter.Sdk.Models
+{
+    public class BMPHeader
+    {
+        public ushort Id { get; set; }
+        public uint FileSize { get; set; }
+        public uint Reserved { get; set; }
+        public uint HeaderSize { get; set; }
+        public uint InfoSize { get; set; }
+        public uint Width { get; set; }
+        public uint Height { get; set; }
+        public ushort Planes { get; set; }
+        public ushort BitsPerPixel { get; set; }
+        public uint Compression { get; set; }
+        public uint ImageSize { get; set; }
+        public uint XPixelsPerMeter { get; set; }
+        public uint YPixelsPerMeter { get; set; }
+        public uint ColorsUsed { get; set; }
+        public uint ColorsImportant { get; set; }
+
+    }
+}
